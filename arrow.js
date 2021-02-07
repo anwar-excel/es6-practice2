bc645cd6acac670bff1c69cec8fafd282258f46b
unknown <anwarhosen575@gmail.com>
2021-02-08 03:14:08 +0600

    first arrow

---
+++ arrow.js
@@ -0,0 +1,15 @@
+// const doubleIt = num => num * 2;
+// const add = (x, y) => x + y;
+// const give5 = () => 4000;
+// const result2 = give5();
+// const result = add(50, 70);
+// console.log(result2);
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const  result = sum * diff;
+    return result ;
+}
+const result3 = doMath(7, 5);
+console.log(result3);

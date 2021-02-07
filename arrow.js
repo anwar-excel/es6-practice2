53a36e21f6187137a437ea07ec596d8264e3f116
unknown <anwarhosen575@gmail.com>
2021-02-08 04:03:21 +0600

    ft

--- arrow.js
+++ arrow.js
@@ -5,11 +5,15 @@
 // const result = add(50, 70);
 // console.log(result2);
 
-const doMath = (x, y) => {
-    const sum = x + y;
-    const diff = x - y;
-    const  result = sum * diff;
-    return result ;
-}
-const result3 = doMath(7, 5);
-console.log(result3);
+// const doMath = (x, y) => {
+//     const sum = x + y;
+//     const diff = x - y;
+//     const  result = sum * diff;
+//     return result ;
+// }
+// const result3 = doMath(7, 5);
+// console.log(result3);
+const ages = [12, 14, 11, 13, 17];
+const ages2 = [15, 16, 12];
+const allAges = ages.concat(ages2);
+console.log(allAges);
---
+++ threedots1.js
@@ -0,0 +1,4 @@
+const ages = [12, 14, 11, 13, 17];
+const ages = [15, 16, 12];
+const allAges = ages.concat(ages2);
+console.log(allAges);
